1925b2f635bd3cfe284851018740dfea548082c1
Taliayaya <ilanvinord@gmail.com>
2022-01-11 12:12:52 +0100

    Correction de bugs

--- MorpionNSIV2.py
+++ MorpionNSIV2.py
@@ -2,56 +2,66 @@
 
 import pygame as pg
 from pygame.constants import MOUSEBUTTONDOWN
-from pygame.cursors import diamond # Forme de curseur
+from pygame.cursors import diamond  # Forme de curseur
 
-#Déclaration de constantes, C is better tho
-COULEUR_FOND = (180,50,0)
-COULEUR_DESSIN = (0,0,255)
-COULEUR_CLIGNO = (0,255,0)
+# Déclaration de constantes, C is better tho
+COULEUR_FOND = (180, 50, 0)
+COULEUR_DESSIN = (0, 0, 255)
+COULEUR_CLIGNO = (0, 255, 0)
 LARGEUR_LIGNE = 8
 DIMENSIONS_GRILLE = 320
 PLACE_GRILLE_VERTI = 220
 PLACE_GRILLE_HORI = 120
 
+
 class Case(pg.Rect):
-    def __init__(self, left, top, width, height, posx = -1, posy = -1):
+    def __init__(self, left, top, width, height, posx=-1, posy=-1):
         self.left = left
         self.top = top
         self.width = width
         self.height = height
         self.lit = False
         self.pos = (posx, posy)
+
     def light(self):
         self.lit = not(self.lit)
-        if self.lit :
-            pg.draw.rect(surf,COULEUR_CLIGNO,((self.left+1,self.top+1),(self.width,self.height)),LARGEUR_LIGNE-5)
-        else :
+        if self.lit:
+            pg.draw.rect(surf, COULEUR_CLIGNO, ((
+                self.left+1, self.top+1), (self.width, self.height)), LARGEUR_LIGNE-5)
+        else:
             self.eteindre()
+
     def eteindre(self):
         self.lit = False
-        pg.draw.rect(surf,COULEUR_DESSIN,((self.left+1,self.top+1),(self.width,self.height)),LARGEUR_LIGNE-5)
-    def dessiner(self, value): #-1 Pour rien, 0 pour Croix, 1 pour Rond
-        pg.draw.rect(surf,COULEUR_FOND,((self.left + 5,self.top + 5),\
-                                        (self.width - LARGEUR_LIGNE,self.height - LARGEUR_LIGNE)),0)
+        pg.draw.rect(surf, COULEUR_DESSIN, ((self.left+1, self.top+1),
+                     (self.width, self.height)), LARGEUR_LIGNE-5)
+
+    def dessiner(self, value):  # -1 Pour rien, 0 pour Croix, 1 pour Rond
+        pg.draw.rect(surf, COULEUR_FOND, ((self.left + 5, self.top + 5),
+                                          (self.width - LARGEUR_LIGNE, self.height - LARGEUR_LIGNE)), 0)
         if not(value):
-            pg.draw.line(surf,COULEUR_DESSIN,(self.topleft[0]+10,self.topleft[1]+10),\
+            pg.draw.line(surf, COULEUR_DESSIN, (self.topleft[0]+10, self.topleft[1]+10),
                          (self.bottomright[0]-10, self.bottomright[1]-10), LARGEUR_LIGNE)
-            pg.draw.line(surf,COULEUR_DESSIN,(self.topright[0]-10,self.topright[1]+10),\
+            pg.draw.line(surf, COULEUR_DESSIN, (self.topright[0]-10, self.topright[1]+10),
                          (self.bottomleft[0]+10, self.bottomleft[1]-10), LARGEUR_LIGNE)
-        elif value == 1 : #A éventuellement transformer en if else booléen si la fonction d'effacement n'est pas utilisée
-            pg.draw.circle(surf,COULEUR_DESSIN,(self.width/2 + self.topleft[0] + 1,\
-                                                self.height/2+ self.topleft[1] + 1),\
-                           (min(self.height,self.widt)/2-6), 5)
+        elif value == 1:  # A éventuellement transformer en if else booléen si la fonction d'effacement n'est pas utilisée
+            pg.draw.circle(surf, COULEUR_DESSIN, (self.width/2 + self.topleft[0] + 1,
+                                                  self.height/2 + self.topleft[1] + 1),
+                           (min(self.height, self.width)/2-6), 5)
+
 
 class Grid():
-    def __init__(self, dimx, dimy, wincond): # A voir si les cases de contrôle et les tours seront gérés ici
-        casex = DIMENSIONS_GRILLE/dimx #Valeurs qui seront peut être changeables à l'avenir
+    # A voir si les cases de contrôle et les tours seront gérés ici
+    def __init__(self, dimx, dimy, wincond):
+        # Valeurs qui seront peut être changeables à l'avenir
+        casex = DIMENSIONS_GRILLE/dimx
         casey = DIMENSIONS_GRILLE/dimy
-        #Cases de contrôle
-        self.case_croix = Case(280,500,80,80) #Positions des cases de contrôle
-        self.case_rond = Case(410,500,80,80)
-        pg.draw.rect(surf,COULEUR_CLIGNO,(280,500,80,80),5)
-        pg.draw.rect(surf,COULEUR_CLIGNO,(410,500,80,80),5)
+        # Cases de contrôle
+        # Positions des cases de contrôle
+        self.case_croix = Case(280, 500, 80, 80)
+        self.case_rond = Case(410, 500, 80, 80)
+        pg.draw.rect(surf, COULEUR_CLIGNO, (280, 500, 80, 80), 5)
+        pg.draw.rect(surf, COULEUR_CLIGNO, (410, 500, 80, 80), 5)
         # Valeurs
         self.wincond = wincond
         self.layout = []
@@ -65,17 +75,18 @@ class Grid():
         start_vertical = PLACE_GRILLE_VERTI
         start_horizontal = PLACE_GRILLE_HORI
         for x in range(dimx+1):
-            self.layout.append([Case(y*casey+PLACE_GRILLE_VERTI, x*casex+PLACE_GRILLE_HORI, casey, casex, x, y)\
+            self.layout.append([Case(y*casey+PLACE_GRILLE_VERTI, x*casex+PLACE_GRILLE_HORI, casey, casex, x, y)
                                 for y in range(dimy)])
-            pg.draw.line(surf, COULEUR_DESSIN,(200, start_horizontal),(560, start_horizontal), LARGEUR_LIGNE)
+            pg.draw.line(surf, COULEUR_DESSIN, (200, start_horizontal),
+                         (560, start_horizontal), LARGEUR_LIGNE)
             start_horizontal = start_horizontal + casex
         for y in range(dimy+1):
-            pg.draw.line(surf, COULEUR_DESSIN,(start_vertical, 100),(start_vertical, 460),LARGEUR_LIGNE)
+            pg.draw.line(surf, COULEUR_DESSIN, (start_vertical, 100),
+                         (start_vertical, 460), LARGEUR_LIGNE)
             start_vertical = start_vertical + casey
-            
 
     def showturn(self, player):
-        if player :
+        if player:
             self.case_croix.dessiner(-1)
             self.case_rond.dessiner(1)
         else:
@@ -89,122 +100,132 @@ class Grid():
         surf.fill(COULEUR_FOND)
 
     def check(self, posx, posy, value):
-        #Cherche si le joueur value a gagné avec le coup joué en (posx, posy)
-        #Renvoie 0 si rien n'est trouvé, et 1 si le joueur est gagnant
+        # Cherche si le joueur value a gagné avec le coup joué en (posx, posy)
+        # Renvoie 0 si rien n'est trouvé, et 1 si le joueur est gagnant
 
-        for i in range(max(0,posy+1-self.wincond), min(self.largeur-self.wincond, posy)+1):
-        #Check les lignes
+        for i in range(max(0, posy+1-self.wincond), min(self.largeur-self.wincond, posy)+1):
+            # Check les lignes
             for j in range(self.wincond):
                 if not(self.positions[posx][i+j] == value):
                     break
-                elif j == self.wincond-1 :
+                elif j == self.wincond-1:
                     return 1
 
-        for i in range(max(0,posx+1-self.wincond), min(self.hauteur-self.wincond, posx)+1):
-        #Check les colonnes
+        for i in range(max(0, posx+1-self.wincond), min(self.hauteur-self.wincond, posx)+1):
+            # Check les colonnes
             for j in range(self.wincond):
                 if not(self.positions[i+j][posy] == value):
                     break
-                elif j == self.wincond-1 :
+                elif j == self.wincond-1:
                     return 1
 
-        #Check la diagonale du haut-gauche au bas-droite
-        #Coordonnées du point auquel la diagonale commence
-        point_debut = (max(posx-min(self.wincond, posy), 0), max(posy-min(self.wincond, posx), 0))
-        i=0
-        while(i+self.wincond<min(self.hauteur-point_debut[0],self.largeur-point_debut[1])+1):
+        # Check la diagonale du haut-gauche au bas-droite
+        # Coordonnées du point auquel la diagonale commence
+        point_debut = (max(posx-min(self.wincond, posy), 0),
+                       max(posy-min(self.wincond, posx), 0))
+        i = 0
+        while(i+self.wincond < min(self.hauteur-point_debut[0], self.largeur-point_debut[1])+1):
             for j in range(self.wincond):
                 if not(self.positions[point_debut[0]+i+j][point_debut[1]+i+j] == value):
                     break
-                elif j== self.wincond-1:
+                elif j == self.wincond-1:
                     return 1
-            i+=1
+            i += 1
 
-        #Check la diagonale du haut-droite au bas-gauche
-        #Coordonnées du point auquel la diagonale commence
-        point_debut = (max(posx-min(self.wincond-1, self.largeur-posy-1), 0),\
-                           min(posy + min(posx, self.wincond-1), self.largeur-1))
-        i=0
-        while(i+self.wincond<min(self.hauteur-point_debut[0],point_debut[1]+1)+1):
+        # Check la diagonale du haut-droite au bas-gauche
+        # Coordonnées du point auquel la diagonale commence
+        point_debut = (max(posx-min(self.wincond-1, self.largeur-posy-1), 0),
+                       min(posy + min(posx, self.wincond-1), self.largeur-1))
+        i = 0
+        while(i+self.wincond < min(self.hauteur-point_debut[0], point_debut[1]+1)+1):
             for j in range(self.wincond):
                 if not(self.positions[point_debut[0]+i+j][point_debut[1]-i-j] == value):
                     break
-                elif j==self.wincond-1:
+                elif j == self.wincond-1:
                     return kuraO
-            i+=1
+            i += 1
 
-        #Rien trouvé
+        # Rien trouvé
         return 0
+
 
 def wait_input() -> int:
     """
     Attend l'entrée d'un entier par l'utilisateur, suivi de la touche espace pour confirmer
     """
-    nombre_actuel=""
-    while True :
+    nombre_actuel = ""
+    while True:
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
-                if event.unicode.isnumeric() :
+                if event.unicode.isnumeric():
                     nombre_actuel += str(event.unicode)
-                    display = smallfont.render(nombre_actuel,False,(0,255,0))
-                    surf.blit(display, (180,250))
+                    display = smallfont.render(
+                        nombre_actuel, False, (0, 255, 0))
+                    surf.blit(display, (180, 250))
                 elif event.key == pg.K_SPACE:
                     if nombre_actuel:
                         return int(nombre_actuel)
                 elif event.key == pg.K_RETURN:
                     pg.quit()
                 elif event.key == pg.K_BACKSPACE:
-                    pg.draw.rect(surf, COULEUR_FOND, ((180,250),(300,400)))
+                    pg.draw.rect(surf, COULEUR_FOND, ((180, 250), (300, 400)))
                     nombre_actuel = nombre_actuel[:-1]
-                    display = smallfont.render(nombre_actuel,False,(0,255,0))
-                    surf.blit(display, (180,250))
-        pg.display.update()                                     
+                    display = smallfont.render(
+                        nombre_actuel, False, (0, 255, 0))
+                    surf.blit(display, (180, 250))
+        pg.display.update()
 
-        
+
 def init():
-    global surf #Le surf est global pour que toutes les fonctions puissent l'utiliser
-    surf = pg.display.set_mode((800,600),0,10)
-    global smallfont #De même pour la police d'affichage
-    pg.font.init() # Active les fonts pour l'écran de nouvelle partie
-    smallfont = pg.font.Font(None,35)
-    pg.display.set_caption("Morpion","Morpion")
+    global surf  # Le surf est global pour que toutes les fonctions puissent l'utiliser
+    surf = pg.display.set_mode((800, 600), 0, 10)
+    global smallfont  # De même pour la police d'affichage
+    pg.font.init()  # Active les fonts pour l'écran de nouvelle partie
+    smallfont = pg.font.Font(None, 35)
+    pg.display.set_caption("Morpion", "Morpion")
     pg.display.set_allow_screensaver(True)
     pg.mouse.set_cursor(diamond)
-    surf.fill(COULEUR_FOND) 
+    surf.fill(COULEUR_FOND)
+
     def Dessiner_options():
         fenetrerejouer = True
         surf.fill(COULEUR_FOND)
-        text = smallfont.render("Pour jouer, entrez <espace>. Pour quitter, entrez <entree>.",False,(0,255,0))
+        text = smallfont.render(
+            "Pour jouer, entrez <espace>. Pour quitter, entrez <entree>.", False, (0, 255, 0))
         while fenetrerejouer:
-            surf.blit(text, (50,200))
+            surf.blit(text, (50, 200))
+            pg.display.update()
             for event in pg.event.get():
                 if event.type == pg.KEYDOWN:
-                     if event.key == pg.K_RETURN:
+                    if event.key == pg.K_RETURN:
                         fenetrerejouer = False
                         pg.quit()
-                     if event.key == pg.K_SPACE:
+                    if event.key == pg.K_SPACE:
                         surf.fill(COULEUR_FOND)
-                        text = smallfont.render("Veuillez entrer le nombre de lignes",False,(0,255,0))
-                        surf.blit(text, (180,200))
+                        text = smallfont.render(
+                            "Veuillez entrer le nombre de lignes", False, (0, 255, 0))
+                        surf.blit(text, (180, 200))
                         hauteur = wait_input()
                         # Récupération du nombre de lignes
                         surf.fill(COULEUR_FOND)
-                        text = smallfont.render("Veuillez entrer le nombre de colonnes",False,(0,255,0))
-                        surf.blit(text, (180,200))
+                        text = smallfont.render(
+                            "Veuillez entrer le nombre de colonnes", False, (0, 255, 0))
+                        surf.blit(text, (180, 200))
                         largeur = wait_input()
                         # Recupération du nombre de colonnes
                         surf.fill(COULEUR_FOND)
-                        text = smallfont.render("Veuillez entrer le nombre de symboles à aligner pour gagner",False,(0,255,0))
-                        surf.blit(text, (50,200))
+                        text = smallfont.render(
+                            "Veuillez entrer le nombre de symboles à aligner pour gagner", False, (0, 255, 0))
+                        surf.blit(text, (50, 200))
                         wincond = wait_input()
                         # Récupération de la condition de victoire
                         surf.fill(COULEUR_FOND)
-                        run(hauteur,largeur,wincond)
-                        text = smallfont.render("Pour rejouer, entrez <espace>. Pour quitter, entrez <entree>.",False,(0,255,0))
-                        surf.blit(text, (50,200))
+                        run(hauteur, largeur, wincond)
+                        text = smallfont.render(
+                            "Pour rejouer, entrez <espace>. Pour quitter, entrez <entree>.", False, (0, 255, 0))
+                        surf.blit(text, (50, 200))
                 if event.type == pg.QUIT:
                     running = False
-            pg.display.update()                                     
     Dessiner_options()
 
 
@@ -212,43 +233,46 @@ def run(hauteur, largeur, wincondition):
     running = True
     grille = Grid(hauteur, largeur, wincondition)
     timer = 0
-    xactuel = 0 #Les coordonnées à mettre à jour
+    xactuel = 0  # Les coordonnées à mettre à jour
     yactuel = 0
-    player_actuel = 0 #Echange entre 0 (croix) et 1 (rond)
+    player_actuel = 0  # Echange entre 0 (croix) et 1 (rond)
     grille.showturn(player_actuel)
     while running:
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_UP:
                     if xactuel:
-                        grille[(xactuel,yactuel)].eteindre()
-                        xactuel -=1
+                        grille[(xactuel, yactuel)].eteindre()
+                        xactuel -= 1
                 elif event.key == pg.K_DOWN:
-                    if xactuel != hauteur-1 :
-                        grille[(xactuel,yactuel)].eteindre()
+                    if xactuel != hauteur-1:
+                        grille[(xactuel, yactuel)].eteindre()
                         xactuel += 1
                 elif event.key == pg.K_RIGHT:
-                    if yactuel != largeur-1 :
-                        grille[(xactuel,yactuel)].eteindre()
+                    if yactuel != largeur-1:
+                        grille[(xactuel, yactuel)].eteindre()
                         yactuel += 1
                 elif event.key == pg.K_LEFT:
                     if yactuel:
-                        grille[(xactuel,yactuel)].eteindre()
+                        grille[(xactuel, yactuel)].eteindre()
                         yactuel -= 1
-                elif event.key == pg.K_TAB:
-                    Dessiner_options()
-                elif event.key == pg.K_SPACE: # Permet de placer sa pièce
-                    if grille.positions[xactuel][yactuel] == -1: #Vérifie que la case est vide
-                        grille[(xactuel,yactuel)].dessiner(player_actuel)
+                # elif event.key == pg.K_TAB:
+                #     Dessiner_options()
+                elif event.key == pg.K_SPACE:  # Permet de placer sa pièce
+                    # Vérifie que la case est vide
+                    print("yup")
+                    if grille.positions[xactuel][yactuel] == -1:
+                        grille[(xactuel, yactuel)].dessiner(player_actuel)
                         grille.positions[xactuel][yactuel] = player_actuel
                         if (grille.check(xactuel, yactuel, player_actuel)):
                             running = False
                             print(f"Victoire du joueur {player_actuel +1} !")
                             surf.fill(COULEUR_FOND)
                             break
-                        player_actuel = abs(1-player_actuel) # Change de joueur
+                        # Change de joueur
+                        player_actuel = abs(1-player_actuel)
                         grille.showturn(player_actuel)
-                elif event.key == pg.K_RETURN: #Permet de réinitialiser la partie
+                elif event.key == pg.K_RETURN:  # Permet de réinitialiser la partie
                     running = False
                     del(grille)
             elif event.type == pg.QUIT:
@@ -258,10 +282,12 @@ def run(hauteur, largeur, wincondition):
         timer += 1
         if timer == 255:
             timer = 0
-            grille[(xactuel,yactuel)].light()
+            grille[(xactuel, yactuel)].light()
+
 
 def main():
     init()
+
 
 if __name__ == "__main__":
     main()
